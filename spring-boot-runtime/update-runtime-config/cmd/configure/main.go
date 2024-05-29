e03e45a9b4f7ba38e886adcf018b95e8591dd933
Vladimir Petko <vladimir.petko@canonical.com>
2024-05-29 18:00:27 +1200

    add spring boot repositories

--- spring-boot-runtime/update-runtime-config/cmd/configure/main.go
+++ spring-boot-runtime/update-runtime-config/cmd/configure/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/canonical/spring-boot-snap/spring-boot-runtime/internal/common"
 	"github.com/canonical/spring-boot-snap/spring-boot-runtime/internal/gradle"
 	"github.com/canonical/spring-boot-snap/spring-boot-runtime/internal/maven"
 )
@@ -16,9 +17,13 @@ func main() {
 }
 
 func run() error {
-	if err := gradle.UpdateInitScripts(); err != nil {
+	repositories := []common.Repository{
+		{Id: "spring-boot", Name: "Spring Boot", Url: "file:///snap/spring-boot-runtime/current/spring-boot/"},
+		{Id: "spring-framework", Name: "Spring Framework", Url: "file:///snap/spring-boot-runtime/current/spring-framework/"},
+	}
+	if err := gradle.UpdateInitScripts(repositories); err != nil {
 		return err
 	}
 
-	return maven.UpdateSettings()
+	return maven.UpdateSettings(repositories)
 }
